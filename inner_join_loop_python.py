40cb9e57b81ddbbd9797334c700012cb2952ec9f
jrostudent <146394427+jrostudent@users.noreply.github.com>
2024-02-01 15:53:34 -0700

    Add files via upload

---
+++ inner_join_loop_python.py
@@ -0,0 +1,45 @@
+#a loop to iterate through a directory containing matrices of kmer rows and sample columns
+#so as to inner join them based on their sample type (i.e PD for progressive disease) 
+#dependent on file nomenclature
+#in code below set to inner join PD_path matrices, easily modifiable for other dirs
+
+import pandas as pd
+import os
+
+PD_path = '/your/PD/dir/here'
+PRCR_path = '/your/PRCR/dir/here'
+
+# Initialize an empty DataFrame for our final result
+matrix_df = None
+
+for directory in [PD_path]:
+    for filename in os.listdir(directory):
+        if filename.endswith('.csv'):
+            
+            # Extract the base name part from the filename (e.g., 'SRR5088813' from 'SRR5088813_Matrix.csv')
+            base_name = os.path.basename(filename).split('_')[0]
+
+            # Construct the full file path
+            file_path = os.path.join(directory, filename)
+
+            # Read the CSV file
+            df = pd.read_csv(file_path)
+
+            # Merge with the result DataFrame
+            if matrix_df is None:
+                matrix_df = df
+            else:
+                matrix_df = pd.merge(matrix_df, df, on='kmer', how='inner')
+                matrix_df.fillna(0, inplace=True)
+
+# Check the final DataFrame
+matrix_df.head()
+
+#print details on full joined matrix
+
+print("matrix has " + str(len(matrix_df)) + " rows ")
+print("Matrix has " + str(matrix_df.shape[1]) + " columns")
+
+#write matrix to .csv
+
+matrix_df.to_csv('/your/destination/dir/here', index=False)
