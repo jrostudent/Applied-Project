28a2f1c8a4e50dd7a35368ca6a9c4d5fdcf3cef4
jrostudent <146394427+jrostudent@users.noreply.github.com>
2024-03-06 15:24:30 -0700

    Update inner_join_loop_python.py
    changed file name and fill NA strategy

--- inner_join_loop_python.py
+++ inner_join_loop_python.py
@@ -30,7 +30,10 @@ for directory in [PD_path]:
                 matrix_df = df
             else:
                 matrix_df = pd.merge(matrix_df, df, on='kmer', how='inner')
-                matrix_df.fillna(0, inplace=True)
+                
+
+#replace NA with 0's
+matrix_df.fillna(0, inplace=True)
 
 # Check the final DataFrame
 matrix_df.head()
@@ -42,4 +45,4 @@ print("Matrix has " + str(matrix_df.shape[1]) + " columns")
 
 #write matrix to .csv
 
-matrix_df.to_csv('/your/destination/dir/here', index=False)
+matrix_df.to_csv('/your/destination/dir/here/file.csv', index=False)
